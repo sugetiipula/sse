162f73d9472bc19d387706672af1750cb0e41236
romergan <126556446+romerganu@users.noreply.github.com>
2023-02-28 11:11:47 +0200

    Update console.js

--- console.js
+++ console.js
@@ -4,3 +4,10 @@ console.log("test")// xd
  xd
 console.log("test")console.log("test")console.log("test")
 console.log("test")// xd
+
+function main () {
+ return null;
+}
+main ()
+
+console.log(main)
