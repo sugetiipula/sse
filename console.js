6347a248b22fcfc98f78c71ad7d5c57bbc9fdcba
romergan <126556446+romerganu@users.noreply.github.com>
2023-02-28 11:10:03 +0200

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,6 @@
 ///best ever console
-console.log("test")
+console.log("test")console.log("test")console.log("test")
+console.log("test")// xd
  xd
+console.log("test")console.log("test")console.log("test")
+console.log("test")// xd
